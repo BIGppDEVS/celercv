c28e8141813f4624fef65dd7c4ff2aa952f6912f
Atharva Rane <atharvarane051102@gmail.com>
2024-02-24 19:31:06 +0530

    setup client side auth flow with protected route

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,55 +1,13 @@
-'use client'
+import { Metadata } from "next";
+import LoginClient from "./client";
 
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+export const metadata: Metadata = {
+  title: "celerCV | Login",
+  description: "Login to your account",
+};
 
-import { Input } from "@/components/ui/input"
-import { Button } from '@/components/ui/button'
+const Login = () => {
+  return <LoginClient />;
+};
 
-export default function Login() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const router = useRouter()
-  const supabase = createClientComponentClient()
-
-  const handleSignUp = async () => {
-    await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${location.origin}/auth/callback`,
-      },
-    })
-    router.refresh()
-  }
-
-  const handleSignIn = async () => {
-    await supabase.auth.signInWithPassword({
-      email,
-      password,
-    })
-    router.refresh()
-  }
-
-  const handleSignOut = async () => {
-    await supabase.auth.signOut()
-    router.refresh()
-  }
-
-  return (
-    <div className='space-y-4'>
-      <h1>Login</h1>
-      <Input name="email" onChange={(e) => setEmail(e.target.value)} value={email} />
-      <Input
-        type="password"
-        name="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
-      <Button onClick={handleSignUp}>Sign up</Button>
-      <Button onClick={handleSignIn}>Sign in</Button>
-      <Button onClick={handleSignOut}>Sign out</Button>
-    </div>
-  )
-}
+export default Login;

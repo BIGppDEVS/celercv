bfe2e463eca3527b724cb434ea61c7731fb94bb1
Atharva Rane <atharvarane051102@gmail.com>
2024-02-25 11:23:27 +0530

    update main container styling

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import ShowcaseSection from "@/components/showcase";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-between m-8 border">
       <Navbar />
       <HeroSection />
       <FeatureGrid />

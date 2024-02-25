6d3671ba14bb09dfd3f78d7d172d74623e2fbd21
Atharva Rane <76894588+Atharane@users.noreply.github.com>
2024-02-25 11:26:46 +0530

    init landing page (#1)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
+import FAQSection from "@/components/faq";
+import FeatureGrid from "@/components/featureGrid";
+import Footer from "@/components/footer";
+import HeroSection from "@/components/hero";
+import Navbar from "@/components/navbar";
+import OnboardingSection from "@/components/onboarding";
+import ShowcaseSection from "@/components/showcase";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between m-8 border">
+      <Navbar />
+      <HeroSection />
+      <FeatureGrid />
+      <OnboardingSection />
+      <ShowcaseSection />
+      <FAQSection />
+      <Footer />
     </main>
   );
 }
